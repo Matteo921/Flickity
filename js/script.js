b058863cfd12c4df27cb69b68006c7b96f2dea96
Mateusz Cieślakowski <cieslakowski.mat@gmail.com>
2019-08-21 13:22:15 +0200

    Done carousel

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
+  freeScroll:true,
   cellAlign: 'left',
   contain: true
+  pageDots: false,
+  draggable: false,
 });
 
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity( '.main-carousel', {
   // options
+  freeScroll: true,
+  contain: true,
+  prevNextButtons: false,
+  
 });
