58ce796718289c2c8d66f7ab5e0113eed3f4fdb2
Mateusz Cieślakowski <cieslakowski.mat@gmail.com>
2019-08-22 23:30:22 +0200

    Correct code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
+(function(){
+
+var templatebox = document.getElementById('template-image-list').innerHTML;
+var templateItem = document.getElementById('template-image-content').innerHTML;
+
+Mustache.parse(templateItem);
+
+var listItem = '';
+	for(var i = 0; i < imageData.length; i++){
+
+		listItem += Mustache.render(templateItem, imageData[i]);
+	}
+
+var fullImageList = Mustache.render(templatebox,{products: listItem});
+results.insertAdjacentHTML('beforeend', fullImageList)
+})();
+// import carousel
+(function(){
+
+var templateSlide = document.getElementById('template-slide').innerHTML;
+
+Mustache.parse(templateSlide);
+
+var slideItem = '';
+	for(var k = 0; k < slideArr.length; k++){
+		
+		slideItem += Mustache.render(templateItem, imageData[k]);
+	}
+
+var fullImageList = Mustache.render(templateSlide,{products: slideItem});
+results2.insertAdjacentHTML('beforeend', fullImageList)
+})();
+
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   freeScroll:true,
   cellAlign: 'left',
-  contain: true
+  contain: true,
   pageDots: false,
   draggable: false,
 });
@@ -17,20 +51,3 @@ var flkty = new Flickity( '.main-carousel', {
   prevNextButtons: false,
   
 });
-// mustache template
-(function(){
-
-var templatebox = document.getElementById('template-image-list').innerHTML;
-var templateItem = document.getElementById('template-image-content').innerHTML
-
-Mustache.parse(templateItem);
-
-var listItem = '';
-	for(var i = 0; i < imageData.length; i++){
-		console.log(imageData);
-		listItem += Mustache.render(templateItem, imageData[i]);
-	}
-
-var fullImageList = Mustache.render(templatebox,{products: ListItem});
-results.insertAdjacentHTML('beforeend', fullImageList)
-})();
